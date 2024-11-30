5c6ca08183a4b879e956bbdfe2309b6969470482
Richard E. <richardaeld@gmail.com>
2024-11-30 19:44:06 +0000

    Added header

--- food-storage-react/src/App.tsx
+++ food-storage-react/src/App.tsx
@@ -1,12 +1,13 @@
 import './App.css'
+import Header from './components/Header'
 import Body from './components/Body'
 
 export default  function App() {
 
   return (
     <>
+      <Header />
       <Body />
-
     </>
   )
 }
---
+++ food-storage-react/src/components/Header.tsx
@@ -0,0 +1,14 @@
+export default function Header() {
+
+   return (
+      <header>
+        <nav>
+          <ul>
+            <li>Home</li>
+            <li>History</li>
+            <li>Storage</li>
+          </ul>
+        </nav>
+      </header>
+   )
+}
